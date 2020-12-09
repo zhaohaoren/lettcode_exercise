39a700671ef4bae16a7811f6784f1e59142f2557
zhaohaoren <zhaohaoren666@gmail.com>
2020-12-09 20:44:24 +0800

    update

---
+++ src/q3_不同路径/Solution.java
@@ -0,0 +1,13 @@
+package q3_不同路径;
+
+class Solution {
+    public static int uniquePaths(int m, int n) {
+        int step = m + n - 2;
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3, 2));
+    }
+}
